dcdfbae53d6cd7d4cf3bec6edf14a18740654314
Apoorv Sharma <sharmavapoorv@gmail.com>
2019-10-14 20:19:17 -0400

    added html templates more routes

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+posts = [
+    {
+        'author': 'Apoorv Sharma',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 25, 2019'
+    },
+    {
+        'author': 'Mike Hannigan',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 14, 2019'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
